96c221c2de64b8335224b2025ad997511063c534
KirtiJha <kirti.jha2025@gmail.com>
2025-03-07 20:39:13 +0530

    add wining logic

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let gameover = new Audio("./assets/gameover.mp3");
 
 let turn = "X";
 
+let isgemeover = false;
+
 // Function to change the turn
 const changeTurn = () => {
     return turn === "X" ? "0" : "X";
@@ -13,7 +15,24 @@ const changeTurn = () => {
 
 // function to check for win
 const checkWin = () => {
+    let boxtext = document.getElementsByClassName("boxtext");
+let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+wins.forEach(e => {
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
+        document.querySelector(".info").innerText = boxtext[e[0]].innerText + "Won";
+        isgemeover = true;
+    }
 
+});
 }
 
 // Game logic
@@ -26,7 +45,10 @@ Array.from(boxes).forEach(element => {
            turn = changeTurn();
             audioTurn.play();
             checkWin();
-            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            if(!isgemeover){
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+            
         }
     })
 })
